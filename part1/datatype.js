46312cd37dd6529555eefcca1de23746fdfb019e
Zeus-here18 <lflank2503@gmail.com>
2025-09-04 13:01:39 +0530

    first commit

---
+++ part1/datatype.js
@@ -0,0 +1,9 @@
+let gameName = "SpiderMan 2";
+
+gameName = "RDR 2";
+console.log(gameName);
+
+const username = "bruce wayne";
+
+username = "Atanu";
+console.log(username);
---
+++ part1/printing.js
@@ -0,0 +1,7 @@
+console.log("hello learner");
+console.log("chai");
+process.stdout.write("chai");
+process.stdout.write("chai");
+
+console.table({ city: "jaipur" });
+console.warn({ city: "jaipur" });
